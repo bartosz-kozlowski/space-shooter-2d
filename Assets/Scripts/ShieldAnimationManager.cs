9a303543fa4074ee153bc478d49a6356cfaed97d
bartosz-kozlowski <barkoz.2003@gmail.com>
2026-06-15 21:00:43 +0200

    Last changes

--- Assets/Scripts/ShieldAnimationManager.cs
+++ Assets/Scripts/ShieldAnimationManager.cs
@@ -2,28 +2,17 @@
 
 /// <summary>
 /// Zarządza wyświetlaniem i aktywacją tarczy ochronnej gracza.
-/// Włącza/wyłącza sprite tarczy i jej collider.
+/// Aktywuje/dezaktywuje obiekt tarczy - Animator startuje automatycznie przy wlaczeniu.
 /// </summary>
 public class ShieldAnimationManager : MonoBehaviour
 {
-    SpriteRenderer shieldSprite;
-    CircleCollider2D shieldCollider;
-
-    void Awake()
-    {
-        shieldSprite = GetComponent<SpriteRenderer>();
-        shieldCollider = GetComponent<CircleCollider2D>();
-    }
-
     public void StartShieldAnimation()
     {
-        shieldSprite.enabled = true;
-        shieldCollider.enabled = true;
+        gameObject.SetActive(true);
     }
 
     public void StopShieldAnimation()
     {
-        shieldSprite.enabled = false;
-        shieldCollider.enabled = false;
+        gameObject.SetActive(false);
     }
 }
